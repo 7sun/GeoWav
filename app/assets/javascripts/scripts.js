72d1b4956556d78c5b9d6737e40b0b013c5a1981
7sun <pakrouse@gmail.com>
2015-02-07 17:09:34 -0800

    experimenting with bootstrap menu on SC page

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,13 +1,13 @@
-function togglePlaylist () {
-	var playlistSelector = document.getElementById("playlist");
-	var iconSelector = document.getElementById("playlist-toggle");
+// function togglePlaylist () {
+// 	var playlistSelector = document.getElementById("playlist");
+// 	var iconSelector = document.getElementById("playlist-toggle");
 
-	if (playlistSelector.className == "show") {
-		playlistSelector.className = "hide";
-		iconSelector.className = "icon-menu3";
-	}
-	else {
-		playlistSelector.className = "show";
-		iconSelector.className = "icon-menu4";
-	}
-}
+// 	if (playlistSelector.className == "show") {
+// 		playlistSelector.className = "hide";
+// 		iconSelector.className = "icon-menu3";
+// 	}
+// 	else {
+// 		playlistSelector.className = "show";
+// 		iconSelector.className = "icon-menu4";
+// 	}
+// }
