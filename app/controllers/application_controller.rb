cd9b74a4257d2ffab286fa880248fa73eb7d1976
mariano89 <marianoe1989@gmail.com>
2015-02-11 17:46:09 -0800

    twitter combined with globe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,17 +4,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
    def index
-   	# @search_results = $twitter_rest.search("#justintimberlake", geocode: "34.03093,-118.26664119999998,500mi", lang: "en").take(10)
+    # @search_results = $twitter_rest.search("#justintimberlake", geocode: "34.03093,-118.26664119999998,500mi", lang: "en").take(10)
     @search = TwitterSearch.add("kanye")
     @tweets = @search.result
-   	# @users = $twitter_rest.user_search("Lebron James")
-   	# @topics = ['obama','bieber']
-    #  	$twitter_stream.filter(track: @topics.join(",")) do |object|
-    #  	 	if object.is_a? (Twitter::Tweet)
-    #  	 		puts object.text
-    #  	 	end
-    #  	 end
-  	end
+    # @users = $twitter_rest.user_search("Lebron James")
+    # @topics = ['obama','bieber']
+    #   $twitter_stream.filter(track: @topics.join(",")) do |object|
+    #     if object.is_a? (Twitter::Tweet)
+    #       puts object.text
+    #     end
+    #    end
+    end
 
 end
 
