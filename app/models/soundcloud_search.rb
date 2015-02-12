bb734b107704cd938004592609710b42347036d2
7sun <pakrouse@gmail.com>
2015-02-11 21:34:33 -0800

    stores tracks in DB with sc users

--- app/models/soundcloud_search.rb
+++ app/models/soundcloud_search.rb
@@ -1,22 +1,41 @@
 class SoundcloudSearch < ActiveRecord::Base
 
   serialize :result, JSON
+  serialize :tracks, JSON
   
   def self.client
     Soundcloud.new(client_id: ENV["soundcloud_client_id"])
   end
 
-  def self.add(string)
-    if search = SoundcloudSearch.find_by(query: string)
+  def self.add(city)
+    if search = SoundcloudSearch.find_by(query: city)
       if search.updated_at < 10.minutes.ago # WAS not created within the last 10 minutes
-        users = client.get('/users', q: string, limit: 100)
-        search.update_attributes(result: users, updated_at: Time.now)
+        users = client.get('/users', q: city, limit: 100)
+        tracks = getTrack(users)
+        search.update_attributes(result: users, tracks: tracks, updated_at: Time.now)
       end
     else #Does NOT exist in DB
-      users = client.get('/users', q: string, limit: 100)
-      search = SoundcloudSearch.create(query: string, result: users)
+      users = client.get('/users', q: city, limit: 100)
+      users = users.sort! { |a, b| b['followers_count'] <=> a['followers_count'] }
+      users.delete_if { |user| user['city'] != city }
+      users = users.take(10)
+      tracks = getTrack(users)
+      search = SoundcloudSearch.create(query: city, result: users, tracks: tracks)
     end
     search
+  end
+
+  def self.getTrack(users)
+    tracks = []
+    users.each do |user|
+      user_track = client.get('/tracks', user_id: user["id"], limit: 1)
+      user_track.first
+      if user_track.first != nil
+        tracks << user_track.first
+      end
+    end
+    puts tracks
+    tracks
   end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150211232500) do
+ActiveRecord::Schema.define(version: 20150212043142) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150211232500) do
     t.string   "result"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "tracks"
   end
 
   create_table "twitter_searches", force: :cascade do |t|
