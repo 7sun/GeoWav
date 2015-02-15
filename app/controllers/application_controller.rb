0dfec61f41a3e2ad9ec0b52b5123715d64800c5d
kelseyljacobsen <kelseyljacobsen@gmail.com>
2015-02-15 12:47:48 -0800

    adds modals for signup and login and styles fonts and colors of modal buttons

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,22 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def authorize
-    redirect_to login_path unless current_user
-  end
-
+  # checks to see there is a user/someone has signed in
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
+    if session[:user_id]
+      @current_user ||= User.find(session[:user_id]) 
+    else 
+    @current_user = nil
+    end 
+    return @current_user
+ end 
 
-  helper_method :current_user
+  # allows access to this method in the view file
+  helper_method :current_user 
+
+  # only allows access to app when user has signed up and signed in
+  def authorize
+    redirect_to root_path unless current_user
+  end 
 
 end
