630e51bf70009b3365d9f88f86b9956632bd769a
7sun <pakrouse@gmail.com>
2015-02-25 19:19:57 -0800

    changes db storgae time for soundcloud to 30

--- app/models/soundcloud_search.rb
+++ app/models/soundcloud_search.rb
@@ -5,7 +5,7 @@ class SoundcloudSearch < ActiveRecord::Base
   
   def self.add(city)
     if search = SoundcloudSearch.find_by(query: city)
-      if search.updated_at < 10.minutes.ago # Was NOT created within the last 10 minutes
+      if search.updated_at < 30.minutes.ago # Was NOT created within the last 30 minutes
         users = client.get('/users', q: city, limit: 100)
         tracks = getTrack(users, city)
         search.update_attributes(result: users, tracks: tracks, updated_at: Time.now)
